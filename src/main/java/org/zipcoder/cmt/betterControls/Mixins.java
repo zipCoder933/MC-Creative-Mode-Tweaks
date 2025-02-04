a202008231ce1d729b0810176f8931cbde0caa86
zipCoder933 <sam.p.w524@gmail.com>
2025-02-04 16:30:34 -0700

    disable flight inertia

---
+++ src/main/java/org/zipcoder/cmt/betterControls/Mixins.java
@@ -0,0 +1,31 @@
+package org.zipcoder.cmt.betterControls;
+
+@SuppressWarnings("CastToIncompatibleInterface")
+public final class Mixins {
+	private Mixins() {}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T me(Object object) {
+		return (T) object;
+	}
+	
+//	public static AccessCameraFields cameraFields(Camera camera) {
+//		return (AccessCameraFields) camera;
+//	}
+//
+//	public static AccessClientPlayerFields clientPlayerFields(LocalPlayer localPlayer) {
+//		return (AccessClientPlayerFields) localPlayer;
+//	}
+//
+//	public static AccessKeyMappingFields keyMappingFields(KeyMapping keyMapping) {
+//		return (AccessKeyMappingFields) keyMapping;
+//	}
+//
+//	public static AccessPlayerFields playerFields(Player player) {
+//		return (AccessPlayerFields) player;
+//	}
+//
+//	public static AccessToggleKeyMappingFields toggleKeyMappingFields(ToggleKeyMapping toggleKeyMapping) {
+//		return (AccessToggleKeyMappingFields) toggleKeyMapping;
+//	}
+}
