a214cbd8433e144d7404a9d90b57d413f75f7c71
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-02-05 08:13:10 -0700

    fixed config and lang

--- src/main/java/org/zipcoder/cmt/Config.java
+++ src/main/java/org/zipcoder/cmt/Config.java
@@ -11,29 +11,31 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 public class Config {
     private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
 
-    private static final ForgeConfigSpec.BooleanValue DISABLE_FLIGHT_INERTIA
-            = COMMON_BUILDER.comment("Whether to disable flight inertia")
-            .define("disableFlightInertia", true);
-
-    //Block reach
-    public static final ForgeConfigSpec.IntValue REACH_MIN_RANGE
-            = COMMON_BUILDER.comment("Minimum reach distance")
-            .defineInRange("minReach", 5, 5, 128);
+    //Common
+    public static final ForgeConfigSpec.IntValue REACH_MIN_RANGE =
+            COMMON_BUILDER.comment("Minimum reach distance")
+                    .defineInRange("common.REACH_MIN_RANGE", 5, 5, 128);
 
     public static ForgeConfigSpec.IntValue REACH_MAX_RANGE =
-            COMMON_BUILDER.comment("Minimum reach distance")
+            COMMON_BUILDER.comment("Maximum reach distance")
                     .defineInRange("common.REACH_MAX_RANGE", 128, 5, 256);
 
-    private static final ForgeConfigSpec.IntValue DEFAULT_REACH_DISTANCE
-            = COMMON_BUILDER.comment("Default reach distance when we enter creative mode")
-            .defineInRange("defaultReach", 64, 5, 256);
+    private static final ForgeConfigSpec.IntValue DEFAULT_REACH_DISTANCE =
+            COMMON_BUILDER.comment("Default reach distance when we enter creative mode")
+                    .defineInRange("common.REACH_DEFAULT_RANGE", 64, 5, 256);
 
-
+    //Client
     public static ForgeConfigSpec.IntValue REPLACE_INTERVAL =
-            COMMON_BUILDER.defineInRange("client.REPLACE_INTERVAL", 1, 1, 256);
+            COMMON_BUILDER.comment("The interval in ticks to run replace tool")
+                    .defineInRange("client.REPLACE_INTERVAL", 1, 1, 256);
 
     public static ForgeConfigSpec.BooleanValue INVERT_REPLACE_LOCK =
-            COMMON_BUILDER.comment("Default: false").define("client.INVERT_REPLACE_LOCK", false);
+            COMMON_BUILDER.comment("If we want to invert replace lock. (Default: false)")
+                    .define("client.INVERT_REPLACE_LOCK", false);
+
+    private static final ForgeConfigSpec.BooleanValue DISABLE_FLIGHT_INERTIA =
+            COMMON_BUILDER.comment("Whether to disable flight inertia")
+                    .define("client.DISABLE_FLIGHT_INERTIA", true);
 
 
     static final ForgeConfigSpec SPEC = COMMON_BUILDER.build();
